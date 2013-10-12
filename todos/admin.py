8ee28eabd05239d73d4473490cce10baf1b11c39
Øyvind Robertsen <oyvind@arcticstyle.net>
2013-10-12 22:52:03 +0200

    more admin fixes

--- todos/admin.py
+++ todos/admin.py
@@ -5,15 +5,16 @@ from todos.models import Todo, Project
 class TodoAdmin(admin.ModelAdmin):
     fieldsets = [
         ('Date information', {'fields': ['pub_date']}),
-        ('Todo info', {'fields': ['description', 'completed']})
+        ('Todo info', {'fields': ['description', 'completed', 'user']})
     ]
-    list_display = ('description', 'pub_date', 'completed')
+    list_display = ('description', 'pub_date', 'completed', 'user')
 
 
 class ProjectAdmin(admin.ModelAdmin):
     fieldsets = [
         ('Date information', {'fields': ['start_date']}),
-        ('Project info', {'fields': ['title']}),
+        ('Project info', {'fields': ['title', 'user']}),
         ]
+    list_display = ('title', 'start_date', 'user')
 
-admin.site.register(Todo, TodoAdmin)
+admin.site.register(Todo, TodoAdmin, ProjectAdmin)
