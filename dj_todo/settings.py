c1d772b784f3119c64d656051607cf95b91918ce
Øyvind Robertsen <oyvind@arcticstyle.net>
2013-10-16 02:18:50 +0200

    trying to fix 500 status on heroku

--- dj_todo/settings.py
+++ dj_todo/settings.py
@@ -173,7 +173,7 @@ DATABASES['default'] = dj_database_url.config()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['djtodos.herokuapp.com']
 
 
 # Static asset configuration
